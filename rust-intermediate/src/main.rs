1fdcea4f848c03a12b730cd61af2f66e9ad5de1c
beamingHigher <nigamsaini12@gmail.com>
2023-04-19 18:50:31 +0530

    structures & ownership

---
+++ rust-intermediate/src/main.rs
@@ -0,0 +1,22 @@
+struct BankAccount {
+    balance: i32,
+    verified: bool
+}
+
+fn print_balance(account: &BankAccount) {
+    println!("{:?}", account.balance);
+}
+
+fn print_verified(account: &BankAccount) {
+    println!("{:?}", account.verified);
+}
+
+fn main() {
+    let my_account = BankAccount {
+        balance: 20,
+        verified: true
+    };
+
+    print_balance(&my_account);
+    print_verified(&my_account);
+}
